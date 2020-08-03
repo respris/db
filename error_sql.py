ca4a8804921fad1a2012f0178f3b5d560921c49f
Rafael Espinar <respinar@cartodb.com>
2020-08-03 19:23:46 +0200

    error module

---
+++ error_sql.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conn = sqlite3.connect("new.db")
+
+cursor = conn.cursor()
+
+try:
+    cursor.execute("insert into populations VALUES('New York City', 'NY', 8400000)")
+    cursor.execute("INSERT INTO populations VALUES('San Francisco', 'CA', 800000)")
+
+    conn.commit()
+except sqlite3.OperationalError:
+    print("Oops! Something went wrong. Try again...")
